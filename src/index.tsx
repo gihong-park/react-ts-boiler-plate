20f3ad4439c6406789a2d47191a019fce9630fa7
MAX-0904 <max@baobabpartners.io>
2022-06-18 14:07:22 +0900

    feat: app add && slice example code

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import "./style/reset.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  HomePage  from "page/homePage/homePage";
+import { Provider } from "react-redux";
+import { store } from "app/store";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,10 +13,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
